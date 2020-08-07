8be5ac3e17cef707483d6ec48e4205c50d25dab9
di-i <55539864+di-i@users.noreply.github.com>
2020-08-07 22:43:56 +0300

    Added owl carousel

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var owl = $('.owl-carousel');
+owl.owlCarousel({
+    items:1,
+    loop:true,
+    margin:20,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplayHoverPause:true,
+    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>']
+
+});
