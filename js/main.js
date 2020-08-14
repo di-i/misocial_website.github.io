ff6be426f3c61dcba71dd207948860e40d90d212
di-i <55539864+di-i@users.noreply.github.com>
2020-08-14 10:08:08 +0300

    Fixed owl-carousel to show multiple sliders

--- js/main.js
+++ js/main.js
@@ -1,11 +1,121 @@
-var owl = $('.owl-carousel');
-owl.owlCarousel({
-    items:1,
-    loop:true,
-    margin:20,
-    autoplay:true,
-    autoplayTimeout:5000,
-    autoplayHoverPause:true,
-    navText : ['<i class="fa fa-angle-left" aria-hidden="true"></i>','<i class="fa fa-angle-right" aria-hidden="true"></i>']
 
-});
+
+
+$(document).ready(function(){
+
+      $(".slide-one").owlCarousel({
+  
+            loop:true, //Зацикливаем слайдер
+            items: 1,
+            //Отступ от картино если выводите больше 1
+            touchDrag: true,
+            nav:false, //Отключил навигацию
+            autoplay:true, //Автозапуск слайдера
+            dots: true,
+            smartSpeed:3000, //Время движения слайда
+  
+            autoplayTimeout:5000, //Время смены слайда
+
+            responsive:{ //Адаптация в зависимости от разрешения экрана
+  
+                0:{
+   
+                    items:1
+  
+                },
+    
+                600:{
+
+                    items:1
+   
+                },
+ 
+                1000:{
+  
+                    items:1
+
+                }
+ 
+            }
+    
+        });
+  
+     
+
+        $(".slide-two").owlCarousel({
+    
+            loop:true, //Зацикливаем слайдер
+            items: 1,
+            margin:0, //Отступ от картино если выводите больше 1
+            touchDrag: true,
+            nav:false, //Отключил навигацию
+
+            autoplay:true, //Автозапуск слайдера
+
+            smartSpeed:2000, //Время движения слайда
+   
+            autoplayTimeout:4000, //Время смены слайда
+   
+            responsive:{ //Адаптация в зависимости от разрешения экрана
+   
+                0:{
+  
+                    items:1
+   
+                },
+  
+                600:{
+ 
+                    items:1
+
+                },
+   
+                1000:{
+
+                    items:1
+   
+                }
+ 
+            }
+ 
+        });
+  
+    });
+
+    $(".slide-three").owlCarousel({
+  
+        loop:true, //Зацикливаем слайдер
+         //Отступ от картино если выводите больше 1
+        dots: true,
+        touchDrag: true,
+        nav:false, //Отключил навигацию
+        autoplay:true, //Автозапуск слайдера
+        lazyLoad: false,
+        smartSpeed:3000, //Время движения слайда
+        stagePadding: 94,
+        autoplayTimeout:5000, //Время смены слайда
+
+        responsive:{ //Адаптация в зависимости от разрешения экрана
+
+            0:{
+
+                items:1
+
+            },
+
+            600:{
+
+                items:2
+
+            },
+
+            1000:{
+
+                items:2
+
+            }
+
+        }
+
+    });
+    
